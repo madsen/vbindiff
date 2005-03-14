08301936169ffc7ce93c291feb662af7a76daebe
Christopher J. Madsen <cjm@cjmweb.net>
2005-03-14 09:39:43 -0600

    Add ability to display EBCDIC instead of ASCII
      Add cmToggleASCII, asciiDisplayTable, & ebcdicDisplayTable

--- vbindiff.cpp
+++ vbindiff.cpp
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------
-// $Id: vbindiff.cpp 4593 2005-03-12 18:24:15Z cjm $
+// $Id: vbindiff.cpp 4594 2005-03-14 15:39:43Z cjm $
 //--------------------------------------------------------------------
 //
 //   Visual Binary Diff
@@ -82,6 +82,7 @@ const Command  cmEditTop      = 8;
 const Command  cmEditBottom   = 9;
 const Command  cmUseTop       = 10;
 const Command  cmUseBottom    = 11;
+const Command  cmToggleASCII  = 12;
 
 const short  leftMar  = 11;     // Starting column of hex display
 const short  leftMar2 = 61;     // Starting column of ASCII display
@@ -98,6 +99,76 @@ const int  maxPath = 260;
 // The number of bytes to move for each possible step size:
 //   See cmmMoveByte, cmmMoveLine, cmmMovePage
 const int  steps[4] = {1, lineWidth, bufSize-lineWidth, 0};
+
+const char asciiDisplayTable[256] = {
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+  0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27,
+  0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F,
+  0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37,
+  0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F,
+  0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47,
+  0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F,
+  0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57,
+  0x58, 0x59, 0x5A, 0x5B, 0x5C, 0x5D, 0x5E, 0x5F,
+  0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67,
+  0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E, 0x6F,
+  0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77,
+  0x78, 0x79, 0x7A, 0x7B, 0x7C, 0x7D, 0x7E,  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.'
+}; // end asciiDisplayTable
+
+const char ebcdicDisplayTable[256] = {
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+  0x20,  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.', 0x2E, 0x3C, 0x28, 0x2B, 0x7C,
+  0x26,  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.', 0x21, 0x24, 0x2A, 0x29, 0x3B, 0x5E,
+  0x2D, 0x2F,  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.', 0x2C, 0x25, 0x5F, 0x3E, 0x3F,
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.', 0x60, 0x3A, 0x23, 0x40, 0x27, 0x3D, 0x22,
+   '.', 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67,
+  0x68, 0x69,  '.',  '.',  '.',  '.',  '.',  '.',
+   '.', 0x6A, 0x6B, 0x6C, 0x6D, 0x6E, 0x6F, 0x70,
+  0x71, 0x72,  '.',  '.',  '.',  '.',  '.',  '.',
+   '.', 0x7E, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78,
+  0x79, 0x7A,  '.',  '.',  '.', 0x5B,  '.',  '.',
+   '.',  '.',  '.',  '.',  '.',  '.',  '.',  '.',
+   '.',  '.',  '.',  '.',  '.', 0x5D,  '.',  '.',
+  0x7B, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47,
+  0x48, 0x49,  '.',  '.',  '.',  '.',  '.',  '.',
+  0x7D, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F, 0x50,
+  0x51, 0x52,  '.',  '.',  '.',  '.',  '.',  '.',
+  0x5C,  '.', 0x53, 0x54, 0x55, 0x56, 0x57, 0x58,
+  0x59, 0x5A,  '.',  '.',  '.',  '.',  '.',  '.',
+  0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37,
+  0x38, 0x39,  '.',  '.',  '.',  '.',  '.',  '.'
+}; // end ebcdicDisplayTable
 
 //====================================================================
 // Class Declarations:
@@ -164,6 +235,7 @@ class Difference
 ConWindow    promptWin,inWin;
 FileDisplay  file1, file2;
 Difference   diffs(&file1, &file2);
+const char*  displayTable = asciiDisplayTable;
 const char*  program_name; // Name under which this program was invoked
 LockState    lockState = lockNeither;
 
@@ -344,7 +416,7 @@ void FileDisplay::display()
       }
       str += sprintf(str, "%02X ", line[i][j]);
 
-      buf[index++] = (line[i][j] >= ' ') ? line[i][j] : '.';
+      buf[index++] = displayTable[line[i][j]];
     }
     memset(buf + index, ' ', sizeof(buf) - index - 1);
     memset(str, ' ', screenWidth - (str - buf2));
@@ -526,7 +598,7 @@ void FileDisplay::setByte(short x, short y, Byte b)
     sprintf(str, "%02X", b);
     win.setAttribs(cFileEdit);
     win.put(leftMar + 3*x + (x / 8), y+1, str);
-    str[0] = (b >= ' ') ? b : '.';
+    str[0] = displayTable[b];
     str[1] = '\0';
     win.put(leftMar2 + x + (x / 8), y+1, str);
     win.setAttribs(cFileWin);
@@ -736,8 +808,10 @@ Command getCommand()
       cmd = cmQuit;
       break;
 
-     case 'B':  cmd = cmUseBottom;  break;
-     case 'T':  cmd = cmUseTop;     break;
+     case 'A':  cmd = cmToggleASCII;  break;
+
+     case 'B':  cmd = cmUseBottom;    break;
+     case 'T':  cmd = cmUseTop;       break;
 
      case KEY_DOWN:   cmd = cmmMove|cmmMoveLine|cmmMoveForward;  break;
      case KEY_RIGHT:  cmd = cmmMove|cmmMoveByte|cmmMoveForward;  break;
@@ -872,6 +946,11 @@ void handleCmd(Command cmd)
     else
       lockState = lockTop;
     displayLockState();
+  }
+  else if (cmd == cmToggleASCII) {
+    displayTable = ((displayTable == asciiDisplayTable)
+                    ? ebcdicDisplayTable
+                    : asciiDisplayTable );
   }
   else if (cmd == cmEditTop)
     file1.edit(&file2);
