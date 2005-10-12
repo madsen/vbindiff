386bbf0ad0111d3d16d1687b91e7d615c7076aa1
Christopher J. Madsen <cjm@cjmweb.net>
2005-10-12 16:56:10 -0500

    Change titleString to include a copyright notice

--- vbindiff.cpp
+++ vbindiff.cpp
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------
-// $Id: vbindiff.cpp 4624 2005-04-13 15:50:25Z cjm $
+// $Id: vbindiff.cpp 4647 2005-10-12 21:56:10Z cjm $
 //--------------------------------------------------------------------
 //
 //   Visual Binary Diff
@@ -39,7 +39,7 @@ using namespace std;
 #include "FileIO.hpp"
 
 const char titleString[] =
-  "\nVBinDiff " PACKAGE_VERSION " by Christopher J. Madsen";
+  "\nVBinDiff " PACKAGE_VERSION "\nCopyright 2005 Christopher J. Madsen";
 
 void exitMsg(int status, const char* message);
 void usage(bool showHelp=true, int exitStatus=0);
