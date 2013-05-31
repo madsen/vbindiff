3b0ea89094c470e181f2aea154f558af3e9a9f34
Bradley Grainger <bgrainger@gmail.com>
2013-05-31 12:26:56 -0700

    Display files up to 64GB.

--- vbindiff.cpp
+++ vbindiff.cpp
@@ -114,8 +114,8 @@ const Command  cmUseBottom    = 11;
 const Command  cmToggleASCII  = 12;
 const Command  cmFind         = 16; // Commands 16-19
 
-const short  leftMar  = 11;     // Starting column of hex display
-const short  leftMar2 = 61;     // Starting column of ASCII display
+const short  leftMar  = 13;     // Starting column of hex display
+const short  leftMar2 = 63;     // Starting column of ASCII display
 
 const int  lineWidth = 16;      // Number of bytes displayed per line
 
@@ -460,7 +460,7 @@ void FileDisplay::display()
   FPos  lineOffset = offset;
 
   short i,j,index,lineLength;
-  char  buf[lineWidth + lineWidth/8 + 1];
+  char  buf[lineWidth + lineWidth/8];
   buf[sizeof(buf)-1] = '\0';
 
   char  buf2[screenWidth+1];
@@ -472,7 +472,7 @@ void FileDisplay::display()
 //    cerr << i << '\n';
     char*  str = buf2;
     str +=
-      sprintf(str, "%04X %04X:",Word(lineOffset>>16),Word(lineOffset&0xFFFF));
+      sprintf(str, "%01X %04X %04X:",Word(lineOffset>>32),Word(lineOffset>>16),Word(lineOffset&0xFFFF));
 
     lineLength  = min(lineWidth, bufContents - i*lineWidth);
 
